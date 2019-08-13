b6dc4e2882b3bb2468785f62206c2d2aa330dc9b
Priya Verma <priya.verma@greatwits.com>
2019-08-13 18:22:00 +0530

    updated blogSettings

--- src/containers/blogSettings/blogSettings.js
+++ src/containers/blogSettings/blogSettings.js
@@ -29,49 +29,41 @@ class BlogSettings extends Component {
         return (
             <Dashboard>
                 <div className="container containerBlogPost">
-                    <h3 className='p-4' style={{ color: "#555555" }}>BLOG POST</h3>
+                    <h3 className='p-4' style={{ color: "#555555" }}>BLOG SETTINGS</h3>
                     <div className="row w-100">
-                        <div className="col-6">
-                            <div className="col-12" >
-                                <div className="md-form active-purple-2 mb-3">
-                                    <h6 style={{ color: "#555555" }}>Create</h6>
+                        <div className="col-5 pl-4">
+                            <div className="col-12" style={{width:"200px"}}>
+                                <div className="md-form border border-top-0 border-right-0 border-left-0 w-21 rounded-0 active-purple-2 mb-3">
+                                    <h6 style={{ color: "#555555" }}>About</h6>
                                 </div>
-                                <div className="md-form active-purple-2 mb-3">
-                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0"
-                                        type="text" style={{ backgroundColor: 'transparent' }} placeholder="Washing very sensitive T-shirts tips & tricks" />
+                                <div className="md-form border border-top-0 border-right-0 border-left-0  rounded-0 active-purple-2 mb-3">
+                                    <h6 style={{ color: "#555555" }}>Social</h6>
                                 </div>
-                                <div style={{ height: "54px" }}></div>
-                                <div className="md-form active-purple-2 mb-3">
-                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Body" />
+                                <div className="md-form border border-top-0 border-right-0 border-left-0  rounded-0 active-purple-2 mb-3">
+                                    <h6 style={{ color: "#555555" }}>Comments</h6>
                                 </div>
-                                <div className="md-form active-purple-2 mb-3">
-                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Author" />
-                                </div>
-                                <div>
-                                    <div className="row">
-                                        <div className="md-form active-purple-2 mb-3 col-4 pl-4">
-                                            <label style={{ color: "#555555" }}>Post Thumbnail</label>
-                                        </div>
-                                        <div className="md-form active-purple-2 mb-3 col-7">
-                                            <label for="file-upload" className="custom-file-upload">CHOOSE</label>
-                                            <label className="ml-2" style={{color:"#888888"}}></label>
-                                            <input id="file-upload" type="file" />
-                                        </div>
-
-                                    </div>
-                                </div>
+                              
+                              
                             </div>
                         </div>
-                        <div className="col-6" style={{ color: "#43425D" }}>
-                            <div className="col-12">
+                        <div className="col-5" style={{ color: "#43425D" }}>
+                            <div className="col-12 ">
                                 <div className="md-form active-purple-2 mb-3">
-                                    <h6 style={{ color: "#555555" }}>SEO</h6>
+                                    <h6 style={{ color: "#555555" }}>About content</h6>
                                 </div>
                                 <div className="md-form active-purple-2 mb-3 row">
-                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0 col-12" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Post URL" />
+                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0 col-12" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Blog Title" />
+                                </div>
+                                <div style={{height:"22px"}}></div>
+                                <div className="md-form active-purple-2 mb-3 row">
+                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0 col-12" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Description" />
+                                </div>
+                                <div style={{height:"22px"}}></div>
+                                <div className="md-form active-purple-2 mb-3 row">
+                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0 col-12" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Blog Description" />
                                 </div>
                                 <div className="md-form active-purple-2 mb-3 row">
-                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0 col-12" type="text" style={{ backgroundColor: 'transparent' }} placeholder="Post Meta D" />
+                                    <input className="form-control border border-top-0 border-right-0 border-left-0 border-dark rounded-0 col-12" type="text" style={{ backgroundColor: 'transparent' }} placeholder="url" />
                                 </div>
                             </div>
                         </div>
