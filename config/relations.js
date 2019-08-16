607bc861a3c6dbe9564832e52dfef40683a53d57
Rohit Khandelwal <rohit.khandelwal@greatwits.com>
2019-08-16 15:16:27 +0530

    updated relations of orders

--- config/relations.js
+++ config/relations.js
@@ -8,14 +8,14 @@ relations.shipments = require('../models/shipment');
 relations.carts = require('../models/cart');
 relations.cartProducts = require('../models/cartProduct');
 
-relations.orders.belongsTo(relations.customers);
-relations.customers.hasMany(relations.orders);
-relations.orders.belongsTo(relations.payments);
-relations.orders.belongsTo(relations.shipments);
-relations.addresses.belongsTo(relations.customers);
-relations.customers.hasMany(relations.addresses);
-relations.shipments.belongsTo(relations.addresses);
-relations.cartProducts.belongsTo(relations.carts);
-relations.orders.belongsTo(relations.carts);
+relations.orders.belongsTo(relations.customers, { foreignKey: 'customerId' });
+relations.customers.hasMany(relations.orders, { foreignKey: 'customerId' });
+relations.orders.belongsTo(relations.payments, { foreignKey: 'paymentId' });
+relations.orders.belongsTo(relations.shipments, { foreignKey: 'shipmentId' });
+relations.addresses.belongsTo(relations.customers, { foreignKey: 'customerId' });
+relations.customers.hasMany(relations.addresses, { foreignKey: 'customerId' });
+relations.shipments.belongsTo(relations.addresses, { foreignKey: 'addressId' });
+relations.cartProducts.belongsTo(relations.carts), { foreignKey: 'cartId' };
+relations.orders.belongsTo(relations.carts, { foreignKey: 'cartId' });
 
 module.exports = relations;
