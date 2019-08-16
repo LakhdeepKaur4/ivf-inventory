711f2afb084d371cec2f5ea5d5a2f6651a35f85e
Rohit Khandelwal <rohit.khandelwal@greatwits.com>
2019-08-16 10:47:38 +0530

    Config-ENV-updated

--- config/env.js
+++ config/env.js
@@ -3,5 +3,11 @@ module.exports = {
         service: 'http://localhost:4000',
         api: 'https://ivf-inventory.herokuapp.com/api',
         brandsAPI: 'http://192.168.1.113:3000/api/brands'
+    },
+    voxel:{
+        service:'Enter URL here'
+    },
+    angel:{
+        service:'Enter URL here'
     }
 }
