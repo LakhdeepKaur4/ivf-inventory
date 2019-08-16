07c4968213c17bf1ce97198a099e8de606e48e26
Rohit Khandelwal <rohit.khandelwal@greatwits.com>
2019-08-16 14:10:12 +0530

    created relational model for orders

--- config/relations.js
+++ config/relations.js
@@ -1,3 +1,17 @@
 const relations = {};
 
+relations.orders = require('../models/order');
+relations.payments = require('../models/payment');
+relations.customers = require('../models/customer');
+relations.addresses = require('../models/address');
+relations.shipments = require('../models/shipment');
+
+relations.orders.belongsTo(relations.customers);
+relations.customers.hasMany(relations.orders);
+relations.orders.belongsTo(relations.payments);
+relations.orders.belongsTo(relations.shipments);
+relations.addresses.belongsTo(relations.customers);
+relations.customers.hasMany(relations.addresses);
+relations.shipments.belongsTo(relations.addresses);
+
 module.exports = relations;
--- models/customer.js
+++ models/customer.js
@@ -4,19 +4,28 @@ const sequelize = require('../config/mysql');
 
 const Customers = sequelize.define('customers',{
     customerId: {
-        type: Sequelize.STRING,
+        type: Sequelize.NUMBER,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
     },
-    firstName: {
+    name: {
         type: Sequelize.STRING
     },
-    lastName: {
+    surname: {
         type: Sequelize.STRING
     },
     email: {
         type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    createdAt: {
+        type: Sequelize.DATE
+    },
+    updatedAt: {
+        type: Sequelize.DATE
     }
 });
 
