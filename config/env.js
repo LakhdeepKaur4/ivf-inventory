c38391fc4496488ba84de7bfc4d19050d7024546
Rohit Khandelwal <rohit.khandelwal@greatwits.com>
2019-08-16 11:07:31 +0530

    Config-ENV-updated

--- config/env.js
+++ config/env.js
@@ -1,13 +1,13 @@
 module.exports = {
 	inventory:{
-        service: 'http://localhost:4000',
+        service: 'http://localhost:4000',//Place inventory server url here
         api: 'https://ivf-inventory.herokuapp.com/api',
         brandsAPI: 'http://192.168.1.113:3000/api/brands'
     },
     voxel:{
-        service:'Enter URL here'
+        service:'Enter URL here'//Place voxel server url here
     },
     angel:{
-        service:'Enter URL here'
+        service:'Enter URL here'//Place angel server url here
     }
 }
