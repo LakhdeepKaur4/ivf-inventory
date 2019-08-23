21ab0eb8d52aed24b26388b2f9b102279c028337
Rohit Khandelwal <rohit.khandelwal@greatwits.com>
2019-08-23 16:02:53 +0530

    added api for advanced search

--- routes/orders.js
+++ routes/orders.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.route("/all").get(controller.getOrders);
 
+router.route("/advancedSearch").get(controller.advancedSearchOrders);
+
 router.route("/:id").get(controller.getOrderById);
 
 router.route("/").post(controller.create);
