03d9da1072bf01533a71580bc6f76e6a9b53b629
Rohit Khandelwal <rohit.khandelwal@greatwits.com>
2019-08-26 18:54:03 +0530

    updated host-resolver component

--- config/env.js
+++ config/env.js
@@ -1,11 +1,6 @@
 module.exports = {
 	inventory:{
-<<<<<<< HEAD
-        service: 'http://192.168.1.117:3003' //Place inventory server url here
-=======
-        service: 'http://192.168.1.113:3000' //Place inventory server url here
-        // service: 'http://localhost:3000' //Place inventory server url here
->>>>>>> 99ae9f22ca3b55ff7a3c7bb9f84d89e9500ec95f
+        service: 'http://locahost:3000' //Place inventory server url here
     },
     voxel:{
         service:'Enter URL here' //Place voxel server url here
