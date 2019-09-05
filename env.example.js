852e567043b4d9c550dfe49406f37ce687e0134f
Shubang Koul <shubang.koul@greatwits.com>
2019-09-05 08:57:45 -0400

    added updated code

--- env.example.js
+++ env.example.js
@@ -1,14 +1,14 @@
 module.exports = {
 	inventory:{
-        service: 'http://192.168.0.110:3003' //Place inventory server url here
+        service: 'Enter URL here' //Place inventory server url here
     },
     voxel:{
-        service:'http://voxel.usingimagination.co.uk' //Place voxel server url here
+        service:'Enter URL here' //Place voxel server url here
     },
     angel:{
         service:'Enter URL here' //Place angel server url here
     },
     mockup:{
-        service: 'http://localhost:4000' //Place mockup server url here
+        service: 'Enter URL here' //Place mockup server url here
     }
 }
