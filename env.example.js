89f0a26cae78d0c1336ddcaa7c40d382367151d6
shubham-rwt <shubham.rawat@greatwits.com>
2019-09-06 17:23:09 +0530

    .env config

--- env.example.js
+++ env.example.js
@@ -1,37 +1,40 @@
 // Environment variables to setup connection
+require('dotenv-safe').config({
+    example: './.env'
+});
 
 module.exports = {
     env: {
-        NODE_ENV: 'Node environment for example production/development',
-        PORT: 'Port to run node app/server'
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: parseInt(process.env.PORT)
     },
     mysql: {
-        database: 'db_name',
-        username: 'db_user',
-        password: 'db_pass',
-        host: 'db_host',
+        database: process.env.MYSQL_DATABASE,
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_password,
+        host: process.env.MYSQL_HOST,
         pool: {
-            max_connections: 'maximum no. of connections in number and not string',
-            min_connections: 'minimum no. of connections in number and not string',
-            connection_acquire_time: 'limit of connection acquire time in number and not string',
-            connection_idle_time: 'limit of connection remains idle time in number and not string'
+            max_connections: parseInt(process.env.MYSQL_MAX_CONNECTIONS),
+            min_connections: parseInt(process.env.MYSQL_MIN_CONNECTIONS),
+            connection_acquire_time: parseInt(process.env.MYSQL_CONNECTION_ACQUIRE_TIME),
+            connection_idle_time: parseInt(process.env.MYSQL_CONNECTION_IDLE_TIME)
         }
     },
     mongo: {
-        MONGO_URI: 'Mongo Connection URI'
+        MONGO_URI: process.env.MONGO_URI
     },
     rabbitmq: {
-        url: 'server URL',
-        port: 'PORT',
-        username: 'server_user',
-        password: 'server_pass'
+        url: process.env.RABBITMQ_URL,
+        port: parseInt(process.env.RABBITMQ_PORT),
+        username: process.env.RABBITMQ_USER_NAME,
+        password: process.env.RABBITMQ_PASSWORD
     },
     minio: {
-        BUCKET_NAME: 'BUCKET_NAME',
-        MINIO_URL: 'server URL',
-        MINIO_PORT: 'PORT',
-        MINIO_USE_SSL: 'true/false',
-        MINIO_ACCESSKEY: 'MINIO_ACCESSKEY',
-        MINIO_SECRETKEY: 'MINIO_SECRETKEY'
+        BUCKET_NAME: process.env.MINIO_BUCKET_NAME,
+        MINIO_URL: process.env.MINIO_URL,
+        MINIO_PORT: parseInt(process.env.MINIO_PORT),
+        MINIO_USE_SSL: process.env.MINIO_USE_SSL == 'true',
+        MINIO_ACCESSKEY: process.env.MINIO_ACCESSKEY,
+        MINIO_SECRETKEY: process.env.MINIO_SECRETKEY
     }
 }
