0d047a9f22fc072d2fa39a90ed09a09daf8e3ddc
Priya Verma <priya.verma@greatwits.com>
2019-09-10 14:47:16 +0530

    updated edit order and customer

--- src/reducers/editCustomerOrderReducer/editCustomerOrderReducer.js
+++ src/reducers/editCustomerOrderReducer/editCustomerOrderReducer.js
@@ -1,6 +1,7 @@
 import {ADD_CUSTOMER,GET_CUSTOMER} from '../../actionCreators/index';
 
 export default function(state={},action){
+    console.log('inside reducer....', action.payload)
     
     switch(action.type){
 
@@ -8,7 +9,8 @@ export default function(state={},action){
         return{...state, add:action.payload}
         
         case GET_CUSTOMER:
-            return{...state, view:action.payload}
+            return{...state, 
+                view:action.payload}
          default:
          return state;
     }
