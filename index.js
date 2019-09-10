77003a41d11545845deb62debe4101bd7b8fad42
Lakhdeep <lakhdeep.kaur@greatwits.com>
2019-09-10 13:01:45 +0530

    updated limit

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ app.use(express.static(__dirname + '/public'));
 
 mongoose.connect();
 app.use(cors());
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-app.use(bodyParser.json({ limit: '500mb' ,extended:true,type:"application/json"}));
-app.use(bodyParser.urlencoded({ limit: '500mb', extended: true,type:'application/x-www-form-urlencoding'}));
+app.use(bodyParser.json({ limit: '100mb' ,extended:true,type:"application/json"}));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true,type:'application/x-www-form-urlencoding'}));
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.use('/public',express.static(path.resolve(__dirname, 'public')));
 app.get('/', function(req, res){
